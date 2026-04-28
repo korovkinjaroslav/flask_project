674d78d58bf84d471d38f05d4df9ac8843305f57
yarkor <korovkin.jaroslav@gmail.com>
2026-04-28 18:40:45 +0400

    posts with blob image

--- main_directory/models/data/posts.py
+++ main_directory/models/data/posts.py
@@ -10,13 +10,16 @@ class Post(SqlAlchemyBase):
                            primary_key=True, autoincrement=True)
     title = sqlalchemy.Column(sqlalchemy.String, nullable=False)
     content = sqlalchemy.Column(sqlalchemy.String, nullable=False)
-    image = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    likes_amount = sqlalchemy.Column(sqlalchemy.Integer, nullable=False, default=0)
+    image = sqlalchemy.Column(sqlalchemy.LargeBinary, nullable=True)
+    likes_amount = sqlalchemy.Column(
+        sqlalchemy.Integer, nullable=False, default=0)
     user_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("users.id"),
                                 nullable=False)
     created_at = sqlalchemy.Column(
         sqlalchemy.DateTime, default=datetime.datetime.now)
 
     user = orm.relationship("User", back_populates="posts")
-    comments = orm.relationship("Comment", back_populates="post", cascade="all, delete-orphan")
-    likes = orm.relationship("Like", back_populates="post", cascade="all, delete-orphan")
+    comments = orm.relationship(
+        "Comment", back_populates="post", cascade="all, delete-orphan")
+    likes = orm.relationship(
+        "Like", back_populates="post", cascade="all, delete-orphan")
