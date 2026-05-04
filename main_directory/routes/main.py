92260a0d058d2db7196d4b7214092e3ef332235f
yarkor <korovkin.jaroslav@gmail.com>
2026-05-04 23:33:15 +0400

    fix + main_page.html

--- main_directory/routes/main.py
+++ main_directory/routes/main.py
@@ -1,9 +1,12 @@
-from flask import Blueprint
+from tempfile import template
 
+from flask import Blueprint, render_template
+
+from main_directory.models.data import posts
 
 main_router = Blueprint("main_router", __name__)
 
 
 @main_router.route("/")
 def start():
-    return "developing"
+    return render_template('main_page.html')
